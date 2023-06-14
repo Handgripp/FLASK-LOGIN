c6fe5ca031f162a4bd0d49a4c8029fc570b78b4b
Kamil Malkowski <kamil.malkowski96@gmail.com>
2023-06-14 18:54:49 +0200

    Add new table and create new endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from controllers import get_all_users, get_one_user, create_user, promote_user, delete_user, update_user, login
+from controllers import get_all_users, get_one_user, create_user, promote_user, delete_user, update_user, login, \
+    get_all_todos, get_one_todo, create_todo, complete_todo, delete_todo
 
 
 def register_routes(app):
@@ -9,6 +10,11 @@ def register_routes(app):
     app.route('/user/<public_id>', methods=['DELETE'])(delete_user)
     app.route('/user', methods=['PUT'])(update_user)
     app.route('/login', methods=['POST'])(login)
+    app.route('/todo', methods=['GET'])(get_all_todos)
+    app.route('/todo/<todo_id>', methods=['GET'])(get_one_todo)
+    app.route('/todo', methods=['POST'])(create_todo)
+    app.route('/todo/<todo_id>', methods=['PUT'])(complete_todo)
+    app.route('/todo/<todo_id>', methods=['DELETE'])(delete_todo)
 
 
 
